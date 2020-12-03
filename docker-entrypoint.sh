9dd09f9a134dae12c2c0c1c85ee0843a2d09a979
Nick <nickbadger1980@googlemail.com>
2020-12-03 14:08:33 +0000

    Ensure  command keeps chars after first delimiter is matched

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,9 +10,9 @@ KUBERNETES_SERVICE_HOST=${KUBERNETES_SERVICE_HOST:-}
 # Grab db-values from vault if vault is enabled and we are running
 # inside kubernetes.
 if [ -n "${KUBERNETES_SERVICE_HOST}" ] && [ "${VAULT_ENABLED}" = "true" ]; then
-  DB_NAME=$(vault-env env | grep DB_NAME | cut -d"=" -f2)
-  DB_USER=$(vault-env env | grep DB_USER | cut -d"=" -f2)
-  DB_PASSWORD=$(vault-env env | grep DB_PASSWORD | cut -d"=" -f2)
+  DB_NAME=$(vault-env env | grep DB_NAME | cut -d"=" -f2-)
+  DB_USER=$(vault-env env | grep DB_USER | cut -d"=" -f2-)
+  DB_PASSWORD=$(vault-env env | grep DB_PASSWORD | cut -d"=" -f2-)
 fi
 
 cat << EOF > "$DB_CONF"
